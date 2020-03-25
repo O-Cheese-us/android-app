c9210d5153f663cd5b33d994c4481870c8ad46ce
Christian Lacek <christian.lacek@gmail.com>
2020-03-25 23:57:45 +0100

    Fut Orsch Beidl zefix, ois is hi!

---
+++ KotlinMessenger/app/src/main/java/letsbuildthatapp/com/kotlinmessenger/HomeActivity.kt
@@ -0,0 +1,89 @@
+package letsbuildthatapp.com.kotlinmessenger
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.content_home.*
+
+private const val TAG = "HomeActivity"
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val db = FirebaseFirestore.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        initCheeseCounter()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
+
+    fun initCheeseCounter() {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val docRef = db.collection("users").document(uid)
+        docRef.get()
+            .addOnSuccessListener { doc ->
+                if (doc != null) {
+                    Log.d(TAG, "Found data : ${doc.data}")
+                    var user = doc.toObject(User::class.java)
+                    if (user?.cheesecounter != null){
+                        edittext_cheesecounter.setText(user?.cheesecounter)
+                    }
+
+                }
+            }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    data class User(
+        var username: String = "",
+        var cheesecounter: Int = 0
+    )
+
+}
--- KotlinMessenger/app/src/main/java/letsbuildthatapp/com/kotlinmessenger/RegisterActivity.kt
+++ KotlinMessenger/app/src/main/java/letsbuildthatapp/com/kotlinmessenger/RegisterActivity.kt
@@ -2,7 +2,6 @@ package letsbuildthatapp.com.kotlinmessenger
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,14 +10,13 @@ import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_register.*
-import java.util.*
+
+private const val TAG = "RegisterActivity"
 
 class RegisterActivity : AppCompatActivity() {
 
-    val db = FirebaseFirestore.getInstance()
-    val TAG = "RegisterActivity"
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,28 +61,42 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun performRegister() {
+
+        val username = username_edittext_register.text.toString()
         val email = email_edittext_register.text.toString()
         val password = password_edittext_register.text.toString()
 
-        if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Email and password are mandatory!", Toast.LENGTH_SHORT).show()
+        // Make sure all required holes were properly filled (WTO)
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Username, Email and password are mandatory!", Toast.LENGTH_SHORT).show()
             return
         }
 
-        Log.d(TAG, "Email is $email")
-        Log.d(TAG, "Password is $password")
-
+        // Check whether an other mighty ocheeseus user with the same name already exists
+        val query = db.collection("users").whereEqualTo("username", username)
+        query.get().addOnSuccessListener {
+                 if (!it.isEmpty){
+                     Log.d(TAG, "Oh ney! Username $username already exists in database!")
+                     Toast.makeText(this, "Creative choice, but unfortunately, another cheese lover is already using that name! Please choose another one.", Toast.LENGTH_SHORT).show()
+                     return@addOnSuccessListener
+                 } else {
+                     Log.d(TAG, "Great! Username $username doesn't exist in database!")
+                 }
+            }
+            .addOnFailureListener {exception -> Log.d(TAG, "Error getting documents: ", exception)
+            }
 
         // Firebase Authentication
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener {
-                if (!it.isSuccessful) {
+                if (!it.isSuccessful || it.result?.user?.uid == null) {
                     return@addOnCompleteListener
+                } else {
+                    val uid = it.result?.user?.uid
+                    Log.d(TAG, "Successfully created user with UID: $uid")
+                    storeUserInFireStore(uid!!, username)
+                    startActivity(Intent(this, HomeActivity::class.java))
                 }
-
-                // else if successful
-                Log.d(TAG, "Successfully created user with UID: ${it.result?.user?.uid}")
-                uploadImageToFirebaseStorage()
             }
             .addOnFailureListener {
                 Log.d(TAG, "Failed to create user: ${it.message}")
@@ -92,52 +104,66 @@ class RegisterActivity : AppCompatActivity() {
             }
     }
 
-    private fun uploadImageToFirebaseStorage() {
+    private fun storeUserInFireStore(uid: String, username: String) {
 
-        Log.d(TAG, "Got selected photo uri: $selectedPhotoUri")
-
-        if (selectedPhotoUri == null) {
-            return
-        }
-
-        val fileName = UUID.randomUUID().toString()
-        val ref = FirebaseStorage.getInstance().getReference("/images/$fileName")
-
-        ref.putFile(selectedPhotoUri!!)
-            .addOnSuccessListener {
-                Log.d(TAG, "Successfully upload image: ${it.metadata?.path}")
-
-                ref.downloadUrl.addOnSuccessListener {
-                    Log.d(TAG, "File Location: $it")
-
-                    saveUserToFirestore(it.toString())
-                }
-
-            }.addOnFailureListener {
-                    e -> Log.w(TAG, "Error writing document", e)
-            }
-
-    }
-
-    private fun saveUserToFirestore(profileImageUrl: String) {
-
-        val uid = FirebaseAuth.getInstance().uid ?: ""
-
-        val user2 = User(uid, username_edittext_register.text.toString(), profileImageUrl, 0)
-
-        val userMap = hashMapOf(
+        val user = hashMapOf(
             "uid" to uid,
             "username" to username_edittext_register.text.toString(),
-            "cheeseCounter" to 0,
-            "profileImageUrl" to profileImageUrl
+            "cheesecounter" to 0
+        )
 
-            )
-
-
-        db.collection("users").document("$uid").set(userMap)
+        db.collection("users").document("$uid").set(user)
             .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written, yo cheese man!")}
-            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+            .addOnFailureListener {
+                    e -> Log.w(TAG, "Error writing document", e)
+                return@addOnFailureListener
+            }
     }
-}
 
-class User(val uid: String, val username: String, val profileImageUrl: String, val cheeseCounter: Int)
+//    private fun uploadImageToFirebaseStorage() {
+//
+//        Log.d(TAG, "Got selected photo uri: $selectedPhotoUri")
+//
+//        if (selectedPhotoUri == null) {
+//            return
+//        }
+//
+//        val fileName = UUID.randomUUID().toString()
+//        val ref = FirebaseStorage.getInstance().getReference("/images/$fileName")
+//
+//        ref.putFile(selectedPhotoUri!!)
+//            .addOnSuccessListener {
+//                Log.d(TAG, "Successfully upload image: ${it.metadata?.path}")
+//
+//                ref.downloadUrl.addOnSuccessListener {
+//                    Log.d(TAG, "File Location: $it")
+//
+//                    saveUserToFirestore(it.toString())
+//                }
+//
+//            }.addOnFailureListener {
+//                    e -> Log.w(TAG, "Error writing document", e)
+//            }
+//
+//    }
+//
+//    private fun saveUserToFirestore(profileImageUrl: String) {
+//
+//        val uid = FirebaseAuth.getInstance().uid ?: ""
+//
+//        val user2 = User(uid, username_edittext_register.text.toString(), profileImageUrl, 0)
+//
+//        val userMap = hashMapOf(
+//            "uid" to uid,
+//            "username" to username_edittext_register.text.toString(),
+//            "cheeseCounter" to 0,
+//            "profileImageUrl" to profileImageUrl
+//
+//            )
+//
+//
+//        db.collection("users").document("$uid").set(userMap)
+//            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written, yo cheese man!")}
+//            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+//    }
+}
