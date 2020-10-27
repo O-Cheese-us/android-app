d2b03b58b8d4023541f9768287540eb620f5e773
Christian Lacek <christian.lacek@gmail.com>
2020-10-27 22:00:12 +0100

    Added basic cheesy cheese tag cheesenator layout.

--- ocheeseus/src/main/java/us/oh/ocheeseus/UsernameActivity.kt
+++ ocheeseus/src/main/java/us/oh/ocheeseus/UsernameActivity.kt
@@ -3,12 +3,17 @@ package us.oh.ocheeseus
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+
+private const val TAG = "CounterActivity"
 
 class UsernameActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_username)
-        Toast.makeText(this, "Griasdi Username!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Griazzdi Username!", Toast.LENGTH_SHORT).show()
     }
 }
